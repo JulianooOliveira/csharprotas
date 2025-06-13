1df7d05547d1330599054e318bd09a6bdaa76597
Juliano <juliano.oliveira1337@gmail.com>
2025-06-13 12:57:41 -0300

    projeto

--- obj/Debug/net8.0/rotasbackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/rotasbackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("rotasbackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d3f1ac73529ff1e79c36363975ee9c0fda35c0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1336a9cb15154592a1172499fc769ffe6a7d8d07")]
 [assembly: System.Reflection.AssemblyProductAttribute("rotasbackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("rotasbackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
